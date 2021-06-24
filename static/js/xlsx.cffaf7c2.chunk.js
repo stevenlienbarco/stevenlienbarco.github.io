8f83edeae13029aee647dc53a606be55e694b329
Lien, Steven <steven.lien@barco.com>
2021-06-24 23:25:14 +0800

    update

--- static/js/xlsx.cffaf7c2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[2],{351:function(n,c){},441:function(n,c){},442:function(n,c){}}]);
-//# sourceMappingURL=xlsx.cffaf7c2.chunk.js.map
---
+++ static/js/xlsx.f1582809.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[2],{390:function(n,c){},475:function(n,c){},476:function(n,c){}}]);
+//# sourceMappingURL=xlsx.f1582809.chunk.js.map
